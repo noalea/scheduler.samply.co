91016a42844f0a04491a7c833530506076fba0eb
Noa Lea <itsmenoalea@gmail.com>
2018-06-12 14:26:57 -0400

    included css & js, added favicon

---
+++ js/scheduler.js
@@ -0,0 +1,12 @@
+// $.ajax({
+//     type: 'GET',
+//     url: 'https://dynacerts.c.go.iopw.com/dyna-fs/get.php'
+// })
+// .done(function(data) {
+//     var d = JSON.parse(data);
+//     // console.log('Data:', d);
+//     organizeData(d);
+// })
+// .fail(function(err) {
+//     console.log(err);
+// });
