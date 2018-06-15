5d12e71edee0eabb271dd3795e01e56e14cc5e32
Noa Lea <itsmenoalea@gmail.com>
2018-06-15 14:05:52 -0400

    retreive all scheduled tweets that have passed or on time

--- php/tweetsCronJob.php
+++ php/tweetsCronJob.php
@@ -8,5 +8,26 @@
 
 $db = require_once 'db/conn.php';
 
-// add schedule info
-echo 'Hi!';
+$day = date("Y-m-d");
+$time = date("h:i:s");
+
+// Find scheduled tweets ($day = TweetSchedule.on_day and $time >= TweetSchedule.on_time)
+$select = "SELECT *
+           FROM TweetSchedule
+           WHERE on_day<='$day' AND on_time<='$time'";
+
+$result = mysqli_query($db, $select);
+while ($row = mysqli_fetch_assoc($result)) {
+    $tsid[] = $row['tsid'];
+    $uid[] = $row['uid'];
+    $consumer_key[] = $row['consumer_key'];
+    $consumer_secret[] = $row['consumer_secret'];
+    $oauth_token[] = $row['oauth_token'];
+    $oauth_token_secret[] = $row['oauth_token_secret'];
+    $status[] = $row['status'];
+    $media[] = $row['media'];
+}
+
+$data = array($tsid, $uid, $consumer_key, $consumer_secret, $oauth_token, $oauth_token_secret, $status, $media);
+
+print_r($data);
