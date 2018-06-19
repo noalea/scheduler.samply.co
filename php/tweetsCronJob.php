f498ad3b600fa76126c4de9a7f27e100d95f0124
Noa Lea <itsmenoalea@gmail.com>
2018-06-19 11:37:48 -0400

    tweets out scheduled tweets

--- php/tweetsCronJob.php
+++ php/tweetsCronJob.php
@@ -6,7 +6,13 @@
  * Time: 3:34 PM
  */
 
+require_once '../twitteroauth/autoload.php';
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+session_start();
+
 $db = require_once 'db/conn.php';
+$config = require_once 'config.php';
 
 $day = date("Y-m-d");
 $time = date("h:i:s");
@@ -15,19 +21,67 @@ $time = date("h:i:s");
 $select = "SELECT *
            FROM TweetSchedule
            WHERE on_day<='$day' AND on_time<='$time'";
-
 $result = mysqli_query($db, $select);
+
 while ($row = mysqli_fetch_assoc($result)) {
     $tsid[] = $row['tsid'];
     $uid[] = $row['uid'];
-    $consumer_key[] = $row['consumer_key'];
-    $consumer_secret[] = $row['consumer_secret'];
-    $oauth_token[] = $row['oauth_token'];
-    $oauth_token_secret[] = $row['oauth_token_secret'];
     $status[] = $row['status'];
-    $media[] = $row['media'];
+    $medias[] = $row['media'];
+    $ontime[] = $row['on_time'];
+
+    $currUID = $row['uid'];
+    $currTSID = $row['tsid'];
+
+    // Find tokens of current user
+    $selectTokens = "SELECT oauth_token, oauth_token_secret
+                     FROM TwitterTokens
+                     WHERE uid='$currUID'";
+    $resultTokens = mysqli_query($db, $selectTokens);
+    $rowTokens = mysqli_fetch_assoc($resultTokens);
+
+    $twitter = new TwitterOAuth(
+        $config['consumer_key'],
+        $config['consumer_secret'],
+        $rowTokens['oauth_token'],
+        $rowTokens['oauth_token_secret']
+    );
+
+    $mediaArr = explode(',', $row['media']);
+
+    $media = array();
+    foreach ($mediaArr as $key => $img) {
+        $url_parts = parse_url($img);
+        $filepath = $_SERVER['DOCUMENT_ROOT'].$url_parts['path'];
+        array_push($media, $twitter->upload('media/upload', ['media' => $filepath]));
+    }
+
+    $media_ids = array();
+    foreach ($media as $m) {
+        array_push($media_ids, $m->media_id_string);
+    }
+
+    $parameters = [
+        'status' => $row['status'],
+        'media_ids' => implode(',', $media_ids)
+    ];
+
+    echo "Parameters:";
+    print_r($parameters);
+
+    $tweet = $twitter->post('statuses/update', $parameters);
+
+    // add status->id & screen_name to db
+    $insert = "INSERT INTO Tweets (uid, tweet_id)
+                  VALUES ('$currUID', '$tweet->id')";
+    mysqli_query($db, $insert);
+
+    $delete = "DELETE FROM TweetSchedule
+               WHERE tsid='$currTSID'";
+    mysqli_query($db, $delete);
 }
 
-$data = array($tsid, $uid, $consumer_key, $consumer_secret, $oauth_token, $oauth_token_secret, $status, $media);
+$data = array($tsid, $uid, $status, $medias, $ontime, $time);
 
+echo "Data: ";
 print_r($data);
