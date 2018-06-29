47b1b8696327c1df4a5df2fe398809f92b010548
Noa Lea <itsmenoalea@gmail.com>
2018-06-29 12:10:45 -0400

    got more info for previewing scheduled tweets

--- php/getScheduledTweets.php
+++ php/getScheduledTweets.php
@@ -12,7 +12,7 @@ $db = require_once 'db/conn.php';
 
 $user = $_COOKIE['screen_name'];
 
-$select = "SELECT S.*
+$select = "SELECT S.*, U.t_username, U.name, U.picture
            FROM TweetSchedule S, Users U
            WHERE U.t_username='$user' AND U.uid=S.uid
            ORDER BY S.on_day, S.on_time";
@@ -24,9 +24,12 @@ while ($row = mysqli_fetch_assoc($result)) {
     $on_day[] = $row['on_day'];
     $on_time[] = $row['on_time'];
     $timezone[] = $row['timezone'];
+    $t_username[] = $row['t_username'];
+    $name[] = $row['name'];
+    $picture[] = $row['picture'];
 }
 
-$arr = array($status, $media, $on_day, $on_time, $timezone);
+$arr = array($status, $media, $on_day, $on_time, $timezone, $t_username, $name, $picture);
 
 
 echo json_encode($arr);
